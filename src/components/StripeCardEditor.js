63e3dbf02ec33ddf223284f81b6265ae9c4d8a22
Samson Ssali <jitpomi@gmail.com>
2019-07-31 03:27:52 -0700

    Stripe Card Editor Added
    Stripe Card Input With grommet included

---
+++ src/components/StripeCardEditor.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import { Box } from "grommet";
+
+import { StripeProvider, Elements } from "react-stripe-elements";
+
+import StripeCardInput from "./StripeCardInput";
+
+export default () => {
+  const [stripe, setStripe] = React.useState(null);
+
+  const [stripeLoaded, setStripeLoaded] = React.useState({});
+
+  const loadStripeScript = src =>
+    new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.addEventListener("load", () => {
+        resolve({ successful: true });
+      });
+      script.addEventListener("error", event => {
+        reject({ error: event });
+      });
+      document.head.appendChild(script);
+    });
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await loadStripeScript("https://js.stripe.com/v3/");
+      setStripeLoaded(result);
+    };
+    fetchData();
+  }, []);
+
+  React.useEffect(() => {
+    if (window.Stripe) {
+      setStripe(window.Stripe("pk_test_TYooMQauvdEDq54NiTphI7jx"));
+    }
+  }, []);
+
+  return (
+    <Box>
+      {stripeLoaded ? (
+        <StripeProvider {...{ stripe }}>
+          <Elements>
+            <StripeCardInput />
+          </Elements>
+        </StripeProvider>
+      ) : null}
+    </Box>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -3,21 +3,33 @@ import ReactDOM from "react-dom";
 import { Grommet, Box, Heading, Text } from "grommet";
 import theme from "./theme";
 import RichTextEditor from "./components/RichTextEditor";
+import { StripeProvider, Elements } from "react-stripe-elements";
 
 import "./styles.css";
+import StripeCardEditor from "./components/StripeCardEditor";
 
 function App() {
   return (
     <Grommet theme={theme} style={{ height: "100vh" }}>
       <Box>
-        <Heading margin={{ bottom: "none" }} level="1">
+        <Heading level="2" margin={{ bottom: "none" }}>
           {" "}
-          RichTextEditor
+          RichText Editor
         </Heading>
         <Text>My 2 cents on rich text editor with image inputs</Text>
       </Box>
       <Box>
         <RichTextEditor />
+      </Box>
+      <Box>
+        <Heading level="2" margin={{ bottom: "none" }}>
+          {" "}
+          Stripe Card Editor
+        </Heading>
+        <Text>My 2 cents on stripe input element </Text>
+      </Box>
+      <Box>
+        <StripeCardEditor />
       </Box>
     </Grommet>
   );
